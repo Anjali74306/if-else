c1653f43da499a8da0adc018aefe021d7ecf2ee5
Anjali74306 <89630072+Anjali74306@users.noreply.github.com>
2021-10-25 16:20:40 +0530

    Add files via upload

---
+++ month.print day.py
@@ -0,0 +1,9 @@
+month=input("enetr the month")
+if month=="february":
+    print(28)
+elif month=="january" or "march" or "may" or "july" or"september" or "november" :
+    print(31)
+else:
+    print(30)
+
+
